cbcc235ceb9014678f114ae95a98d23456156b33
RobertoMitsunari <roberto.mitsunari06@gmail.com>
2025-11-05 00:26:28 -0300

    Projeto preparado para publish

--- ScrollsTracker.Api/Program.cs
+++ ScrollsTracker.Api/Program.cs
@@ -1,79 +1,86 @@
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Migrations.Internal;
 using ScrollsTracker.Api.AutoMapperProfile;
 using ScrollsTracker.Api.Config;
 using ScrollsTracker.Application.Reference;
 using ScrollsTracker.Infra.Repository.Context;
 
-var builder = WebApplication.CreateBuilder(args);
 
-var provider = builder.Configuration["DatabaseProvider"];
-string? connectionString;
-
-if (string.IsNullOrEmpty(provider))
+internal class Program
 {
-    throw new InvalidOperationException("Provider do banco não encontrada");
+	private static void Main(string[] args)
+	{
+		var builder = WebApplication.CreateBuilder(args);
+
+		var provider = builder.Configuration["DatabaseProvider"];
+		string? connectionString;
+
+		if (string.IsNullOrEmpty(provider))
+		{
+			throw new InvalidOperationException("Provider do banco não encontrada");
+		}
+
+		switch (provider)
+		{
+			case "Sqlite":
+				connectionString = builder.Configuration.GetConnectionString("Sqlite");
+				break;
+
+			case "SqlServer":
+				connectionString = builder.Configuration.GetConnectionString("SqlServer");
+				break;
+
+			default:
+				throw new InvalidOperationException($"Database provider '{provider}' não é suportado.");
+		}
+
+		if (string.IsNullOrEmpty(connectionString))
+		{
+			throw new InvalidOperationException("ConnectionString do banco não encontrada");
+		}
+
+		builder.Services.AddConfigRepository(connectionString, provider);
+
+		builder.Services.AddLogging();
+		builder.Services.AddCorsConfig();
+		builder.Services.AddControllers();
+		builder.Services.AddConfigService();
+		builder.Services.AddConfigFacade();
+		builder.Services.AddConfigJob();
+		builder.Services.AddEndpointsApiExplorer();
+		builder.Services.AddSwaggerGen();
+
+		builder.Services.AddAutoMapper(typeof(AutoMapperProfile));
+
+		builder.Services.AddMediatR(cfg =>
+		{
+			cfg.RegisterServicesFromAssembly(typeof(Program).Assembly);
+			cfg.RegisterServicesFromAssembly(typeof(ApplicationAssemblyReference).Assembly);
+		});
+
+		var app = builder.Build();
+
+		using (var scope = app.Services.CreateScope())
+		{
+			var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+			db.Database.Migrate();
+		}
+
+		app.UseCors("PermitirFrontend");
+
+		app.UseHttpsRedirection();
+		app.UseAuthorization();
+		app.MapControllers();
+		app.UseSwagger();
+		app.UseSwaggerUI();
+
+
+		// Serve arquivos estáticos (React build)
+		app.UseDefaultFiles();   // Procura index.html por padrão
+		app.UseStaticFiles();    // Serve arquivos de wwwroot
+
+		// Fallback para index.html (rotas SPA)
+		app.MapFallbackToFile("index.html");
+
+		app.Run();
+	}
 }
-
-switch (provider)
-{
-	case "Sqlite":
-		connectionString = builder.Configuration.GetConnectionString("Sqlite");
-		break;
-
-	case "SqlServer":
-		connectionString = builder.Configuration.GetConnectionString("SqlServer");
-		break;
-
-	default:
-		throw new InvalidOperationException($"Database provider '{provider}' não é suportado.");
-}
-
-if (string.IsNullOrEmpty(connectionString))
-{
-	throw new InvalidOperationException("ConnectionString do banco não encontrada");
-}
-
-builder.Services.AddConfigRepository(connectionString, provider);
-
-builder.Services.AddLogging();
-builder.Services.AddCorsConfig();
-builder.Services.AddControllers();
-builder.Services.AddConfigService();
-builder.Services.AddConfigFacade();
-builder.Services.AddConfigJob();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddAutoMapper(typeof(AutoMapperProfile));
-
-builder.Services.AddMediatR(cfg => {
-	cfg.RegisterServicesFromAssembly(typeof(Program).Assembly);
-	cfg.RegisterServicesFromAssembly(typeof(ApplicationAssemblyReference).Assembly);
-});
-
-var app = builder.Build();
-
-//using (var scope = app.Services.CreateScope())
-//{
-//	var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-//	db.Database.Migrate();
-//}
-
-app.UseCors("PermitirFrontend");
-
-//app.UseHttpsRedirection();
-app.UseAuthorization();
-app.MapControllers();
-app.UseSwagger();
-app.UseSwaggerUI();
-
-
-// Serve arquivos estáticos (React build)
-app.UseDefaultFiles();   // Procura index.html por padrão
-app.UseStaticFiles();    // Serve arquivos de wwwroot
-
-// Fallback para index.html (rotas SPA)
-app.MapFallbackToFile("index.html");
-
-app.Run();
---
+++ ScrollsTracker.Infra/Migrations/SqliteMigrations/20251105022952_InitialSqlite.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ScrollsTracker.Infra.Migrations.SqliteMigrations
+{
+    /// <inheritdoc />
+    public partial class InitialSqlite : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Obras",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Titulo = table.Column<string>(type: "TEXT", nullable: false),
+                    Descricao = table.Column<string>(type: "TEXT", nullable: true),
+                    TotalCapitulos = table.Column<string>(type: "TEXT", nullable: true),
+                    UltimoCapituloLido = table.Column<string>(type: "TEXT", nullable: true),
+                    Imagem = table.Column<string>(type: "TEXT", nullable: true),
+                    Status = table.Column<string>(type: "TEXT", nullable: true),
+                    StatusLeitura = table.Column<string>(type: "TEXT", nullable: true),
+                    DataAtualizacao = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    DataVerificacao = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Obras", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Obras_Titulo",
+                table: "Obras",
+                column: "Titulo",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Obras");
+        }
+    }
+}
